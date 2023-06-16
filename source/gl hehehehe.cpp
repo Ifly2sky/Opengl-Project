62c4e69286800bf6b49fc0d58acbbba342ab6cbb
tino hautanen <tinhau24@gmail.com>
2023-06-16 17:31:20 +0300

    added vertex data

--- source/gl hehehehe.cpp
+++ source/gl hehehehe.cpp
@@ -18,6 +18,7 @@
 #include <thread>
 #include <chrono>
 #include <array>
+#include <objects/VertexData.h>
 
 using namespace std::chrono_literals;
 
@@ -27,7 +28,7 @@ Camera* camera = new Camera(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0
 
 int width = 400, height = 400;
 bool isFullScreen = false;
-const int refreshrate = 100;
+const int refreshrate = 100000000;
 float  deltaTime, lastFrame;
 
 GLFWwindow* window;
@@ -61,22 +62,29 @@ int main()
 
     OnLoad();
 
-
     /*std::cout << file.ReadAllText("shader.vert") << "\n";
     std::cout << file.ReadAllText("shader.frag") << "\n";*/
     shader = new Shader(file.ReadAllText("shader.vert"), file.ReadAllText("shader.frag"));
 
-    /*float vertices[] = {
-        -0.5f,  -0.5f, 0.0f,  0.0f, 0.0f,    // top right
-         0.5f, -0.5f, 0.0f,   1.0f, 0.0f,   // bottom right
-         0.0f, 0.5f, 0.0f,   0.5f, 1.0f,   // bottom left
-    };*/
     float vertices[] = {
         0.5f,  0.5f, 0.0f, 1.0f, 1.0f, // top right
         0.5f, -0.5f, 0.0f, 1.0f, 0.0f, // bottom right
         -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, // bottom left
         -0.5f,  0.5f, 0.0f, 0.0f, 1.0f // top left 
     };
+    /*Vertex v1;
+    v1.Position = { 0.5f, 0.5f, 0.0f };
+    v1.TexCoordinates = { 1.0f, 1.0f };
+    Vertex v2;
+    v2.Position = { 0.5f, -0.5f, 0.0f };
+    v2.TexCoordinates = { 1.0f, 0.0f };
+    Vertex v3;
+    v3.Position = { -0.5f, -0.5f, 0.0f };
+    v3.TexCoordinates = { 0.0f, 0.0f };
+    Vertex v4;w
+    v3.Position = { -0.5f,  0.5f, 0.0f };
+    v3.TexCoordinates = { 0.0f, 1.0f };*/
+
     unsigned int indices[] = {  // note that we start from 0!
         0, 1, 3,   // first triangle
         1, 2, 3    // second triangle
---
+++ source/objects/VertexData.h
@@ -0,0 +1,8 @@
+#include <array>
+
+struct  Vertex
+{
+    std::array<float, 3>Position;
+    std::array<float, 2>TexCoordinates;
+};
+
